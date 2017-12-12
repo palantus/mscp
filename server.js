24294c4ed151989dba228e91a94c3a9a59bf80da
Anders Houbak Kristiansen <palantus@gmail.com>
2017-12-12 16:05:39 +0100

    Fixed file upload of large files

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ class Server{
       app = this.parentMSCP.server
     } else {
       app = express()
+      app.use(fileUpload())
       app.use(bodyParser.urlencoded({ extended: true }))
       app.use(bodyParser.json())
-      app.use(fileUpload())
       app.use(cookieParser());
       app.use(async (req, res, next) => await this.security.onRequest.call(this.security, req, res, next));
     }
